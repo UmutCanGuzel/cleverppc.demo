e0469fcdbfe091f855d39548d5f243fd0ea698bd
Selen Dilek <yildirimselen17@gmail.com>
2023-09-27 18:17:25 -0400

    US-07 DONE

--- src/test/java/Pages/DialogContent.java
+++ src/test/java/Pages/DialogContent.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class DialogContent extends Parent{
     public DialogContent() {
         PageFactory.initElements(GWD.getDriver(),this);
@@ -25,14 +27,263 @@ public class DialogContent extends Parent{
 
     //US7 locators
 
+    @FindBy(name = "email_create")
+    public WebElement emailAdress;
+
+    @FindBy(name = "SubmitCreate")
+    public WebElement submitCreate;
+
+    @FindBy(id = "id_gender1")
+    public WebElement gender_1;
+
+    @FindBy(id = "customer_firstname")
+    public WebElement customer_firstname;
+
+    @FindBy(id = "customer_lastname")
+    public WebElement customer_lastname;
+
+
+    @FindBy(id = "email")
+    public WebElement email;
+
+    @FindBy(id = "passwd")
+    public WebElement passwd;
+
+    @FindBy(id = "days")
+    public WebElement days;
+
+    @FindBy(id = "uniform-days")
+    public WebElement daysClick;
+    @FindBy(id = "months")
+    public WebElement months;
+
+    @FindBy(id = "uniform-months")
+    public WebElement monthsClick;
+
+    @FindBy(id = "uniform-years")
+    public WebElement yearsClick;
+
+    @FindBy(id = "years")
+    public WebElement years;
+
+    @FindBy(id = "submitAccount")
+    public WebElement submitAccount;
+
+    @FindBy(name = "SubmitLogin")
+    public WebElement submitLogin;
+
+    @FindBy(linkText = "Sign in")
+    public WebElement signIn;
+
+    @FindBy(xpath = "//ul[@class='toggle-footer']//li//a[@title='Sitemap']")
+    public WebElement siteMap;
+
+    @FindBy(xpath = "//a[@title='View a list of my addresses']")
+    public WebElement addresses;
+    @FindBy(xpath = "//a[@title='Add an address']")
+    public WebElement add_Address;
+
+
+
+    @FindBy(id = "firstname")
+    public WebElement firstname;
+
+    @FindBy(id = "lastname")
+    public WebElement lastname;
+
+    @FindBy(id = "company")
+    public WebElement company;
+
+    @FindBy(id = "address1")
+    public WebElement address1;
+    @FindBy(id = "address2")
+    public WebElement address2;
+    @FindBy(id = "city")
+    public WebElement city;
+    @FindBy(id = "postcode")
+    public WebElement postcode;
+
+    @FindBy(id = "phone")
+    public WebElement phone;
+
+    @FindBy(id = "phone_mobile")
+    public WebElement phone_mobile;
+    @FindBy(id = "other")
+    public WebElement other;
+    @FindBy(name = "submitAddress")
+    public WebElement submitAddress;
+    @FindBy(id = "uniform-id_state")
+    public WebElement stateClick;
+
+    @FindBy(id = "id_state")
+    public WebElement stateSelect;
+
+    @FindBy(id = "uniform-id_country")
+    public WebElement countryClick;
+
+    @FindBy(id = "id_country")
+    public WebElement countrySelect;
+
+    @FindBy(id = "alias")
+    public WebElement alias;
+
+    @FindBy(xpath = "//a//img[@class='replace-2x img-responsive']")
+    public List<WebElement> img_mouseovers;
+
+    @FindBy(xpath = "//a[@title='Add to cart']//span")
+    public List<WebElement> addToCards;
+
+    @FindBy(xpath = "//div[@class='button-container']//span[@title='Continue shopping']")
+    public WebElement countinueButton;
+
+    @FindBy(xpath = "//a[@title='Proceed to checkout']")
+    public WebElement checkOutButton;
+
+    @FindBy(xpath = "(//a[@title='Dresses'])[2]")
+    public WebElement dressess;
+
+    @FindBy(xpath = "//*[@id=\"form\"]/div/div[4]/div/div/p/label")
+    public WebElement agreeButton;
+
+    @FindBy(xpath = "(//a[@title='Proceed to checkout'])[2]")
+    public WebElement proceedButton;
+
+    @FindBy(name = "processAddress")
+    public WebElement proceedButton2;
+    @FindBy(name = "processCarrier")
+    public WebElement proceedButton3;
+
+    @FindBy(xpath = "(//span[@class='checked'])[2]")
+    public WebElement checked;
+    //
+    @FindBy(id = "paypal-standard-btn")
+    public WebElement paypalButton;
+
+    @FindBy(xpath = "//div[@class='content-wrap']//div[@class='message']")
+    public WebElement paypalMessage;
+    @FindBy(xpath = "(//p[@class='payment_module']//a)[3]")
+    public WebElement creditCardButton;
+
+    @FindBy(xpath = "/html/body")
+    public WebElement creditCardMsg;
+
+    @FindBy(xpath = "(//p[@class='payment_module']//a)[1]")
+    public WebElement bankButton;
+
+    @FindBy(xpath = "//td[@class='cart_total']//span")
+    public List<WebElement> totalPriceList;
+
+    @FindBy(xpath = "(//td[@class='price'])[1]")
+    public WebElement totalProducs;
+
+    @FindBy(xpath = "(//td[@class='price'])[2]")
+    public WebElement totalShipping;
+
+    @FindBy(xpath = "(//td[@class='price'])[3]")
+    public WebElement TOTAL;
+
+    @FindBy(xpath = "//span[@id='amount']")
+    public WebElement amount;
+
+    @FindBy(xpath = "(//button[@type='submit'])[2]")
+    public WebElement confirmButton;
+
+    @FindBy(xpath = "//p[@class='alert alert-success']")
+    public WebElement completeMessage;
+
+
+
     //US8 locators
 
 
+
+
+
+
+
     public WebElement getWebElement(String strElement){
-        switch (strElement){
-            // case "addButton": return this.addButton;
+        switch (strElement) {
+            case "emailAdress":
+                return this.emailAdress;
+            case "submitCreate":
+                return this.submitCreate;
+            case "gender_1":
+                return this.gender_1;
+            case "customer_firstname":
+                return this.customer_firstname;
+            case "customer_lastname":
+                return this.customer_lastname;
+            case "email":
+                return this.email;
+            case "passwd":
+                return this.passwd;
+            case "days":
+                return this.days;
+
+            case "months":
+                return this.months;
+
+            case "years":
+                return this.years;
+            case "submitLogin":
+                return this.submitLogin;
+            case "firstname":
+                return this.firstname;
+            case "lastname":
+                return this.lastname;
+            case "company":
+                return this.company;
+            case "address1":
+                return this.address1;
+            case "address2":
+                return this.address2;
+            case "city":
+                return this.city;
+            case "postcode":
+                return this.postcode;
+            case "phone":
+                return this.phone;
+            case "phone_mobile":
+                return this.phone_mobile;
+            case "other":
+                return this.other;
+            case "stateClick":
+                return this.stateClick;
+            case "countryClick":
+                return this.countryClick;
+            case "alias":
+                return this.alias;
+            case "agreeButton":
+                return this.agreeButton;
+            case "proceedButton":
+                return this.proceedButton;
+            case "paypalButton":
+                return this.paypalButton;
+            case "paypalMessage":
+                return this.paypalMessage;
+            case "creditCardButton":
+                return this.creditCardButton;
+            case "creditCardMsg":
+                return this.creditCardMsg;
+
+            case "bankButton":
+                return this.bankButton;
+
+            case "proceedButton2":
+                return this.proceedButton2;
+            case "proceedButton3":
+                return this.proceedButton3;
+            case "signIn":
+                return this.signIn;
+            case "siteMap":
+                return this.siteMap;
+            case "addresses":
+                return this.addresses;
+            case "add_Address":
+                return this.add_Address;
 
         }
+
 
         return null;
     }
--- src/test/java/StepDefinitions/_07_CheckoutSteps.java
+++ src/test/java/StepDefinitions/_07_CheckoutSteps.java
@@ -1,4 +1,179 @@
 package StepDefinitions;
 
+import Pages.DialogContent;
+import Utilities.GWD;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
+import java.util.List;
+
 public class _07_CheckoutSteps {
+
+    DialogContent dc = new DialogContent();
+    WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(10));
+
+    @Given("The users navigate to Cleverppc homepage")
+    public void theUsersNavigateToCleverppcHomepage() {
+
+        GWD.getDriver().get("https://cleverppc.com/prestashop4/");
+
+
+    }
+
+    @When("The users click the sign in button")
+    public void theUsersClickTheSignInButton() {
+        dc.myClick(dc.signIn);
+
+
+    }
+
+    @And("Enter the current e-mail and password")
+    public void enterTheCurrentEMailAndPassword(DataTable dt) {
+        List<List<String>> list = dt.asLists(String.class);
+        for (int i = 0; i < list.size(); i++) {
+            WebElement e = dc.getWebElement(list.get(i).get(0));
+            dc.mySendKeys(e, list.get(i).get(1));
+
+        }
+
+    }
+
+    @And("The users click the submit in button")
+    public void theUsersClickTheSubmitInButton() {
+        dc.myClick(dc.submitLogin);
+    }
+
+
+
+
+    @When("The users click the dress button")
+    public void theUsersClickTheDressButton() {
+        dc.myClick(dc.dressess);
+    }
+
+
+    @Then("The users choose three diffirent clothes in this section")
+    public void theUsersChooseThreeDiffirentClothesInThisSection() {
+
+        List<WebElement> listOfCards = dc.addToCards;
+        List<WebElement> listOfIMG = dc.img_mouseovers;
+
+        Actions actions = new Actions(GWD.getDriver());
+
+        int a = 0;
+        for (int i = 0; i < listOfIMG.size(); i++) {
+            int x = dc.randomGenerator(listOfIMG.size());
+            actions.moveToElement(listOfIMG.get(x)).build().perform();
+            wait.until(ExpectedConditions.elementToBeClickable(listOfCards.get(x)));
+            dc.myClick(listOfCards.get(x));
+            a++;
+            if (a < 3) {
+
+                dc.myClick(dc.countinueButton);
+            } else {
+                dc.myClick(dc.checkOutButton);
+
+                Assert.assertTrue(GWD.getDriver().getCurrentUrl().contains("controller"));
+            }
+
+        }
+
+    }
+
+    @When("The users click the buttons")
+    public void theUsersClickTheButtons(DataTable dt) {
+
+        List<String> list = dt.asList(String.class);
+        for (int i = 0; i < list.size(); i++) {
+
+            dc.myClick(dc.getWebElement(list.get(i)));
+
+
+        }
+
+        dc.myClick(dc.agreeButton);
+        wait.until(ExpectedConditions.visibilityOf(dc.checked));
+        dc.myClick(dc.proceedButton3);
+
+    }
+
+    @Then("When  The users click the buttons")
+    public void whenTheUsersClickTheButtons(DataTable dt) {
+
+        SoftAssert _softassert = new SoftAssert();
+        Double actualTotal = 0.0;
+        //hesap islemleri buraya...
+        List<WebElement> listOfproducts = dc.totalPriceList;
+        for (int i = 0; i < listOfproducts.size(); i++) {
+            JavascriptExecutor js = (JavascriptExecutor) GWD.getDriver();
+            js.executeScript("arguments[0].scrollIntoView(true);", listOfproducts.get(i));
+            Double product_Fee = Double.parseDouble(listOfproducts.get(i).getText().trim().replace("$", ""));
+
+            actualTotal += (double) product_Fee;
+        }
+
+        System.out.println("actualTotal = " + actualTotal);
+
+        JavascriptExecutor js = (JavascriptExecutor) GWD.getDriver();
+        js.executeScript("arguments[0].scrollIntoView(true);", dc.totalProducs);
+        wait.until(ExpectedConditions.visibilityOf(dc.totalProducs));
+        Double total_payment = Double.parseDouble(dc.totalProducs.getText().trim().replace("$", ""));
+        System.out.println("total_payment = " + total_payment);
+
+        js.executeScript("arguments[0].scrollIntoView(true);", dc.totalShipping);
+        wait.until(ExpectedConditions.visibilityOf(dc.totalShipping));
+        Double total_shipping = Double.parseDouble(dc.totalShipping.getText().trim().replace("$", ""));
+        System.out.println("total_shipping = " + total_shipping);
+
+        js.executeScript("arguments[0].scrollIntoView(true);", dc.TOTAL);
+        wait.until(ExpectedConditions.visibilityOf(dc.TOTAL));
+        Double total_TOTAL = Double.parseDouble(dc.TOTAL.getText().trim().replace("$", ""));
+        System.out.println("total_TOTAL = " + total_TOTAL);
+
+
+        _softassert.assertEquals(total_TOTAL, (total_payment + total_shipping));
+
+        int a = 0;
+        List<String> list = dt.asList(String.class);
+        for (int i = 0; i < list.size(); i++) {
+            dc.myClick(dc.getWebElement(list.get(i)));
+
+            if (a == 0) {
+
+                System.out.println("mp.paypalMessage.getText() = " + dc.paypalMessage.getText());
+
+                _softassert.assertTrue(dc.paypalMessage.getText().contains("try again"));
+                GWD.getDriver().navigate().back();
+            } else if (a == 1) {
+
+                System.out.println("mp.creditCardMsg.getText() = " + dc.creditCardMsg.getText());
+                _softassert.assertTrue(dc.creditCardMsg.getText().contains("Invalid"));
+                GWD.getDriver().navigate().back();
+            } else {
+
+                System.out.println("mp.amount.getText() = " + dc.amount.getText());
+                Double amount_Double = Double.parseDouble(dc.amount.getText().trim().replace("$", ""));
+                _softassert.assertEquals(amount_Double, total_TOTAL);
+                dc.myClick(dc.confirmButton);
+
+                _softassert.assertTrue(dc.completeMessage.getText().contains("complete"));
+            }
+            a++;
+            _softassert.assertAll();
+
+        }
+    }
+
+
 }
