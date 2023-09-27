fb67479664b07f01c6ca347995d417a52ad4872f
RustamRoz <rustamrozik626@gmail.com>
2023-09-27 09:40:52 +0300

    us-5

--- src/test/java/Pages/DialogContent.java
+++ src/test/java/Pages/DialogContent.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class DialogContent extends Parent{
     public DialogContent() {
         PageFactory.initElements(GWD.getDriver(),this);
@@ -20,6 +22,26 @@ public class DialogContent extends Parent{
     //US4 locators
 
     //US5 locators
+    @FindBy(xpath="//*[@id='header']/div[2]/div/div/nav/div[1]/a")
+    public WebElement signIN;
+    @FindBy(id="email")
+    public WebElement username;
+
+    @FindBy(id="passwd")
+    public WebElement password;
+
+    @FindBy(xpath="//*[@id='SubmitLogin']/span")
+    public WebElement loginButton;
+
+    @FindBy(xpath="//*[@id='block_top_menu']/ul/li[2]")
+    public WebElement dresses;
+
+    @FindBy(xpath="//*[@id='center_column']//*[contains(text(), 'Showing 1 - 5 of 5 items')]")
+    public WebElement containText;
+    @FindBy(xpath="//span[@class='heading-counter']")
+    public WebElement fiveProduct;
+    @FindBy(xpath="//*[@id='product_list']//img")
+    public List<WebElement> items;
 
     //US6 locators
 
